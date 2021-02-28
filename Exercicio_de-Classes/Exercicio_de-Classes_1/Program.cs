17eb2373185e15f7e99f47bc5f5c0a2806ca741f
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-28 15:14:16 -0300

    Add files via upload

---
+++ Exercicio_de-Classes/Exercicio_de-Classes_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercicio_de_Classes_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pessoa pessoa = new Pessoa();
+            Pessoa pessoa1 = new Pessoa();
+
+            Console.WriteLine("Dados da Primeira Pessoa :");
+            Console.Write("Nome : ");
+            pessoa.nome = Console.ReadLine();
+            Console.Write("Idade : ");
+            pessoa.idade = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Dados da Segunda Pessoa :");
+            Console.Write("Nome : ");
+            pessoa1.nome = Console.ReadLine();
+            Console.Write("Idade : ");
+            pessoa1.idade = int.Parse(Console.ReadLine());
+
+            if(pessoa.idade > pessoa1.idade)
+            {
+                Console.WriteLine("A Pessoa mais velha é : " + pessoa.nome);
+            }
+            else
+            {
+                Console.WriteLine("A Pessoa mais velha é : " + pessoa1.nome);
+            }
+        }
+    }
+}
